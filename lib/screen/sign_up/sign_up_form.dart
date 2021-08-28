701834861edf403aada4d90b5aee291a682d5ec7
WeiLun <wayne90040@gmail.com>
2021-08-29 01:48:06 +0800

    simple sign-up with firebase

--- lib/screen/sign_up/sign_up_form.dart
+++ lib/screen/sign_up/sign_up_form.dart
@@ -9,7 +9,6 @@ import 'package:flutter_ecommerce/widget/default_button.dart';
 import 'package:flutter_ecommerce/widget/form_error.dart';
 import 'package:provider/provider.dart';
 
-import '../../constants.dart';
 import '../../size_config.dart';
 
 class SignUpForm extends StatefulWidget {
@@ -25,26 +24,11 @@ class _SignUpFormState extends State<SignUpForm> {
   late String confirmPassword;
   final List<String> errors = [];
 
-  void addError({required String error}) {
-    if (!errors.contains(error)) {
-      setState(() {
-        errors.add(error);
-      });
-    }
-  }
-
-  void removeError({required String error}) {
-    if (errors.contains(error)) {
-      setState(() {
-        errors.remove(error);
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     var viewModel = Provider.of<SignUpViewModel>(context);
+
     return Form(
         key: _formKey,
         child: Column(
@@ -54,35 +38,18 @@ class _SignUpFormState extends State<SignUpForm> {
             _buildPasswordTextFormField(),
             SizedBox(height: getProportionateScreenHeight(30)),
             _buildConfirmPassTextFormField(),
-            FormError(errors: errors),
+            FormError(errors: viewModel.errors),
             SizedBox(height: getProportionateScreenHeight(40)),
             DefaultButton(
               text: "Continue",
               onTapped: () {
                 _formKey.currentState!.save();
-                viewModel.registrationWithEmail(email: email, password: password, confirmPassword: confirmPassword);
 
-                switch (viewModel.status) {
-                  case LoadingStatus.loading:
-                  // TODO: Handle this case.
-                    print("isLoading");
-                    break;
-                  case LoadingStatus.none:
-                  // TODO: Handle this case.
-                    print("none");
-                    break;
-                  case LoadingStatus.error:
-                  // TODO: Handle this case.
-                    print("error: 2");
-                    break;
-                  case LoadingStatus.success:
-                  // TODO: Handle this case.
-                    print("done");
+                viewModel.registrationWithEmail(email: email, password: password, confirmPassword: confirmPassword).then((result) {
+                  if (result) {
                     Navigator.pushNamed(context, CompleteProfileScreen.routeName);
-                    break;
-                }
-                if (_formKey.currentState != null && _formKey.currentState!.validate()) {
-                }
+                  }
+                });
               }
             )
           ],
@@ -90,30 +57,10 @@ class _SignUpFormState extends State<SignUpForm> {
     );
   }
 
-
   TextFormField _buildConfirmPassTextFormField() {
     return TextFormField(
       obscureText: true,
       onSaved: (value) => confirmPassword = value ?? "",
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kPassNullError);
-        } else if (value.isNotEmpty && password == confirmPassword) {
-          removeError(error: kMatchPassError);
-        }
-        confirmPassword = value;
-      },
-      validator: (value) {
-        String strongValue = value ?? "";
-        if (strongValue.isEmpty) {
-          addError(error: kPassNullError);
-          return "";
-        } else if ((password != strongValue)) {
-          addError(error: kMatchPassError);
-          return "";
-        }
-        return null;
-      },
       decoration: InputDecoration(
           labelText: "Confirm Password",
           hintText: "Re-enter your password",
@@ -129,25 +76,6 @@ class _SignUpFormState extends State<SignUpForm> {
     return TextFormField(
       obscureText: true,
       onSaved: (value) => password = value ?? "",
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kPassNullError);
-        } else if (value.length >= 8) {
-          removeError(error: kShortPassError);
-        }
-        password = value;
-      },
-      validator: (value) {
-        String strongValue = value ?? "";
-        if (strongValue.isEmpty) {
-          addError(error: kPassNullError);
-          return "";
-        } else if (strongValue.length < 8) {
-          addError(error: kShortPassError);
-          return "";
-        }
-        return null;
-      },
       decoration: InputDecoration(
         labelText: "Password",
         hintText: "Enter your password",
@@ -163,25 +91,6 @@ class _SignUpFormState extends State<SignUpForm> {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
       onSaved: (value) => email = value!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kEmailNullError);
-        } else if (emailValidatorRegExp.hasMatch(value)) {
-          removeError(error: kInvalidEmailError);
-        }
-        return null;
-      },
-      validator: (value) {
-        String strongValue = value ?? "";
-        if (strongValue.isEmpty) {
-          addError(error: kEmailNullError);
-          return "";
-        } else if (!emailValidatorRegExp.hasMatch(strongValue)) {
-          addError(error: kInvalidEmailError);
-          return "";
-        }
-        return null;
-      },
       decoration: InputDecoration(
           labelText: "Email",
           hintText: "Enter your email",
--- lib/viewmodels/sign_up_view_model.dart
+++ lib/viewmodels/sign_up_view_model.dart
@@ -3,79 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_ecommerce/constants.dart';
 
-
-enum LoadingStatus {
-  loading, none, error, success
-}
-
 class SignUpViewModel extends ChangeNotifier {
 
-  late LoadingStatus _status;
-  LoadingStatus get status => _status;
-
-  late List<String> _errors;
+  List<String> _errors = [];
   List<String> get errors => _errors;
-
-
-  Future<void> registrationWithEmail({
-    required String email,
-    required String password,
-    required String confirmPassword }) async {
-    
-    if (!_validEmail(email: email)) {
-      return;
-    }
-
-    if (!_validPassword(password: password, confirmPassword: confirmPassword)) {
-      return;
-    }
-    
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-      _status = LoadingStatus.success;
-    } on FirebaseException catch (e) {
-      _status = LoadingStatus.error;
-    } catch (e) {
-      _status = LoadingStatus.error;
-    }
-  }
-
-  bool _validEmail({required String email}) {
-    if (email.isEmpty) {
-      _addError(error: kEmailNullError);
-      return false;
-    } else {
-      _removeError(error: kEmailNullError);
-      if (emailValidatorRegExp.hasMatch(email)) {
-        _addError(error: kInvalidEmailError);
-        return false;
-      } else {
-        _removeError(error: kInvalidEmailError);
-      }
-    }
-
-    return true;
-  }
-
-  bool _validPassword({required String password, required String confirmPassword}) {
-    if (password.isEmpty) {
-      _addError(error: kPassNullError);
-      return false;
-    } else {
-      _removeError(error: kPassNullError)
-      if (password.length < 8) {
-        _addError(error: kShortPassError);
-        return false;
-      } else {
-        _removeError(error: kShortPassError);
-        if (password != confirmPassword) {
-          _addError(error: kMatchPassError);
-          return false;
-        }
-      }
-    }
-    return true;
-  }
 
   void _addError({required String error}) {
     if (!_errors.contains(error)) {
@@ -91,5 +22,66 @@ class SignUpViewModel extends ChangeNotifier {
     }
   }
 
+  Future<bool> registrationWithEmail({
+    required String email,
+    required String password,
+    required String confirmPassword }) async {
+    
+    if (!_validEmail(email: email)) {
+      return Future.value(false);
+    }
 
+    if (!_validPassword(password: password, confirmPassword: confirmPassword)) {
+      return Future.value(false);
+    }
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+      return Future.value(true);
+    } on FirebaseException catch (e) {
+      // catch error
+    } catch (e) {
+      // catch error
+    }
+    return Future.value(false);
+  }
+
+  bool _validEmail({required String email}) {
+    if (email.isEmpty) {
+      _addError(error: kEmailNullError);
+      return false;
+    } else {
+      _removeError(error: kEmailNullError);
+      if (!emailValidatorRegExp.hasMatch(email)) {
+        _addError(error: kInvalidEmailError);
+        return false;
+      } else {
+        _removeError(error: kInvalidEmailError);
+      }
+    }
+    return true;
+  }
+
+  bool _validPassword({required String password, required String confirmPassword}) {
+    if (password.isEmpty) {
+      _addError(error: kPassNullError);
+      return false;
+    } else {
+      _removeError(error: kPassNullError);
+      if (password.length < 8) {
+        _addError(error: kShortPassError);
+        return false;
+      } else {
+        _removeError(error: kShortPassError);
+        if (password != confirmPassword) {
+          _addError(error: kMatchPassError);
+          return false;
+        }
+        else {
+          _removeError(error: kMatchPassError);
+        }
+      }
+    }
+    return true;
+  }
 }
