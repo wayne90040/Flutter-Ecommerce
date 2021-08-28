9945fd59ac94b180816f07b432100f8c98ec4ad0
WeiLun <wayne90040@gmail.com>
2021-08-29 01:48:44 +0800

    simple sign in with firebase

--- lib/screen/sign_in/sign_in_screen.dart
+++ lib/screen/sign_in/sign_in_screen.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/screen/sign_in/body.dart';
+import 'package:flutter_ecommerce/viewmodels/sign_in_view_model.dart';
+import 'package:provider/provider.dart';
 
 class SignInScreen extends StatelessWidget {
   static String routeName = "/sign_in";
@@ -9,11 +11,16 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign in'),
+    return ChangeNotifierProvider(
+      create: (BuildContext context) {
+        return SignInViewModel();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Sign in'),
+        ),
+        body: Body(),
       ),
-      body: Body(),
     );
   }
 }
