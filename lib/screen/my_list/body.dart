8361510431744865d5c4775012340daf76b1ae8d
WeiLun <wayne90040@gmail.com>
2021-10-11 20:25:46 +0800

    🚧 my_list ui developer

--- lib/screen/my_list/body.dart
+++ lib/screen/my_list/body.dart
@@ -1,16 +1,105 @@
 
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/constants.dart';
+import 'package:flutter_ecommerce/models/my_list_model.dart';
+import 'package:flutter_ecommerce/size_config.dart';
+import 'package:flutter_ecommerce/viewmodels/my_list_view_model.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<MyListViewModel>(context, listen: false).getMyListInFirebase();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     // TODO: implement build
-    return SingleChildScrollView(
-      child: ListView.builder(
-          itemCount: 10,
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount:2,
+            childAspectRatio: 250 / MediaQuery.of(context).size.width,
+            crossAxisSpacing: 10
+          ),
+          itemCount: Provider.of<MyListViewModel>(context).myListModel?.products.length ?? 0,
           itemBuilder: (context, index) {
-            return Container();
+            return SizedBox(
+              child: Consumer<MyListViewModel>(
+                builder: (context, viewModel, child) {
+                  Product? model = viewModel.myListModel?.products[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 1,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: kSecondaryColor.withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(15)
+                          ),
+                          child: Image.asset("assets/images/ps4_console_white_1.png"),
+                        ),
+                      ),
+
+                      Text(
+                        model?.brandName ?? '',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Text(
+                        model?.productName ?? '',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$${model?.price ?? 0}",
+                            style: TextStyle(
+                                fontSize: getProportionateScreenWidth(18),
+                                fontWeight: FontWeight.bold,
+                                color: kPrimaryColor
+                            ),
+                          ),
+
+                          InkWell(
+                            borderRadius: BorderRadius.circular(30),
+                            onTap: () {
+                              print("Tapped Add MyList");
+                            },
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: kPrimaryColor.withOpacity(0.15),
+                                shape: BoxShape.circle
+                              ),
+                              child: SvgPicture.asset(
+                                "assets/icons/Heart Icon_2.svg",
+                                color: Color(0xFFFF4848),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  );
+                },
+              ),
+            );
           }
       ),
     );
--- lib/screen/my_list/my_list_screen.dart
+++ lib/screen/my_list/my_list_screen.dart
@@ -2,18 +2,26 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/screen/my_list/body.dart';
+import 'package:flutter_ecommerce/viewmodels/my_list_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MyListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("MyList"),
-        elevation: 3,
+    return ChangeNotifierProvider(
+      create: (BuildContext context) {
+        return MyListViewModel();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("MyList"),
+          elevation: 3,
+        ),
+        body: Body(),
       ),
-      body: Container(),
     );
   }
 }
